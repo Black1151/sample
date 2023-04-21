1f5d4155e693bb45f92fa119c3b81b1445d9d91e
blackPrawn <danblack9988@gmail.com>
2023-04-21 05:50:46 +0100

    refactor contact form for readability and add email field

--- sample/pages/contact.tsx
+++ sample/pages/contact.tsx
@@ -6,28 +6,31 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Textarea,
   VStack,
   Select,
   useToast,
+  FormErrorMessage
 } from '@chakra-ui/react';
-import { Formik, Form, Field, FieldInputProps } from 'formik';
+import { Formik, Form, Field, FieldInputProps, FormikErrors, FormikTouched } from 'formik';
 import * as Yup from 'yup';
 
 interface FormValues {
-    title: string;
-    name: string;
-    surname: string;
-    telephone: string;
-    message: string;
-    [key: string]: string; 
-};
-  
+  title: string;
+  name: string;
+  surname: string;
+  telephone: string;
+  message: string;
+  [key: string]: string;
+}
+
 const initialValues: FormValues = {
   title: '',
   name: '',
   surname: '',
   telephone: '',
   message: '',
+  email: '',
 };
 
 const ContactSchema = Yup.object().shape({
@@ -35,12 +38,22 @@ const ContactSchema = Yup.object().shape({
   name: Yup.string().required('Name is required'),
   surname: Yup.string().required('Surname is required'),
   telephone: Yup.string()
-    .matches(/^\d+$/, 'Must be a valid phone number')
+    .matches(/^(\+?\d{2,3})?\d+$/, 'Must be a valid phone number')
     .required('Required'),
-    message: Yup.string().required('Message is required'),
+  email: Yup.string()
+    .email('Must be a valid email')
+    .required('Required'),
+  message: Yup.string().required('Message is required'),
 });
 
-const fields = [
+interface FieldConfig {
+  name: string;
+  label: string;
+  type: string;
+  options?: { value: string; label: string }[];
+}
+
+const fields: FieldConfig[] = [
   {
     name: 'title',
     label: 'Title',
@@ -55,8 +68,66 @@ const fields = [
   { name: 'name', label: 'Name', type: 'text' },
   { name: 'surname', label: 'Surname', type: 'text' },
   { name: 'telephone', label: 'Telephone', type: 'tel' },
+  { name: 'email', label: 'Email', type: 'email' },
   { name: 'message', label: 'Message', type: 'textarea' },
 ];
+
+const renderFormControl = (
+  fieldConfig: FieldConfig,
+  field: FieldInputProps<string>,
+  errors: FormikErrors<FormValues>,
+  touched: FormikTouched<FormValues>
+) => {
+  switch (fieldConfig.type) {
+    case 'select':
+      return (
+        <>
+          <Select {...field}>
+            <option value="" disabled>
+              Select your {fieldConfig.label.toLowerCase()}
+            </option>
+            {fieldConfig.options?.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </Select>
+          {errors[fieldConfig.name] && touched[fieldConfig.name] && (
+            <FormErrorMessage>{errors[fieldConfig.name]}</FormErrorMessage>
+          )}
+        </>
+      );
+    case 'textarea':
+      return (
+        <>
+          <Textarea
+            {...field}
+            id={fieldConfig.name}
+            placeholder={fieldConfig.label}
+            isInvalid={!!(errors[fieldConfig.name] && touched[fieldConfig.name])}
+          />
+          {errors[fieldConfig.name] && touched[fieldConfig.name] && (
+            <FormErrorMessage>{errors[fieldConfig.name]}</FormErrorMessage>
+          )}
+        </>
+      );
+    default:
+      return (
+        <>
+          <Input
+            {...field}
+            id={fieldConfig.name}
+            type={fieldConfig.type}
+            placeholder={fieldConfig.label}
+            isInvalid={!!(errors[fieldConfig.name] && touched[fieldConfig.name])}
+          />
+          {errors[fieldConfig.name] && touched[fieldConfig.name] && (
+            <FormErrorMessage>{errors[fieldConfig.name]}</FormErrorMessage>
+          )}
+        </>
+      );
+  }
+};
 
 const ContactUs: React.FC = () => {
   const toast = useToast();
@@ -82,59 +153,39 @@ const ContactUs: React.FC = () => {
           onSubmit={onSubmit}
         >
           {({ errors, touched, isValid }) => (
-            <Form>
-              <VStack spacing={4}>
-                {fields.map((fieldConfig) => (
-                  <Field key={fieldConfig.name} name={fieldConfig.name}>
-                    {({ field }: { field: FieldInputProps<string> }) => (
-                      <FormControl
-                      isInvalid={!!(errors[fieldConfig.name] && touched[fieldConfig.name])}
-                      id={fieldConfig.name}
-                    >
-                        <FormLabel htmlFor={fieldConfig.name}>
-                          {fieldConfig.label}
-                        </FormLabel>
-                        {fieldConfig.type === 'select' ? (
-                          <Select {...field}>
-                            <option value="" disabled>
-                              Select your {fieldConfig.label.toLowerCase()}
-                            </option>
-                            {fieldConfig.options?.map((option) => (
-                              <option
-                                key={option.value}
-                                value={option.value}
-                              >
-                                {option.label}
-                              </option>
-                            ))}
-                          </Select>
-                        ) : (
-                          <Input
-                            {...field}
-                            id={fieldConfig.name}
-                            type={fieldConfig.type}
-                            placeholder={fieldConfig.label}
-                          />
-                        )}
-                      </FormControl>
-                    )}
-                  </Field>
-                ))}
-                <Button
-                  type="submit"
-                  colorScheme="blue"
-                  mt={4}
-                  isDisabled={!isValid}
-                >
-                  Submit
-                </Button>
-              </VStack>
-            </Form>
-          )}
-        </Formik>
-      </Box>
-    </Container>
-  );
+        <Form>
+          <VStack spacing={4}>
+            {fields.map((fieldConfig) => (
+              <Field key={fieldConfig.name} name={fieldConfig.name}>
+                {({ field }: { field: FieldInputProps<string> }) => (
+                  <FormControl
+                    isInvalid={!!(errors[fieldConfig.name] && touched[fieldConfig.name])}
+                    id={fieldConfig.name}
+                  >
+                    <FormLabel htmlFor={fieldConfig.name}>
+                      {fieldConfig.label}
+                    </FormLabel>
+                    {renderFormControl(fieldConfig, field, errors, touched)}
+                  </FormControl>
+                )}
+              </Field>
+            ))}
+            <Button
+              type="submit"
+              colorScheme="blue"
+              mt={4}
+              isDisabled={!isValid}
+            >
+              Submit
+            </Button>
+          </VStack>
+        </Form>
+      )}
+    </Formik>
+  </Box>
+</Container>
+
+);
 };
 
 export default ContactUs;
